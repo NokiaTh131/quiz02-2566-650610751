8c51faad177ad01ca23996afce4729c09c4678cc
= <=>
2023-08-10 14:39:24 +0700

    EZ

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,25 @@
 "use client";
 
+import { Comment } from "@/components/Comment";
+import { PostOwner } from "@/components/PostOwner";
+import { Reply } from "@/components/Reply";
+import { comments } from "@/libs/comments";
+
+// let USER = [
+//   {
+//     userImagePath: "/profileImages/lisa.jpg",
+//     username: "Lisa",
+//     commentText: "จริงค่า",
+//     likeNum: "999",
+//   },
+//   {
+//     userImagePath: "/profileImages/charliebrown.jpg",
+//     username: "Charlie Brown",
+//     commentText: "Crazyyy!!",
+//     likeNum: "0",
+//   },
+// ];
+
 export default function HomePage() {
   return (
     <div
@@ -12,82 +32,42 @@ export default function HomePage() {
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
         {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
-            </span>
-          </div>
-
-          <span className="text-white">
-            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
-          </span>
-
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span style={{ color: "#B0B3B8" }}>100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
+        <PostOwner
+          firstname="Jakkapong"
+          lastname="Jinasen"
+          id="650610751"
+          image="/profileImages/roob.jpg"
+          post="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207"
+          like="100"
+        />
 
         {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div>
+        {/* <Comment
+          userImagePath={"/profileImages/lisa.jpg"}
+          username={"Lisa"}
+          commentText={"จริงค่า"}
+          likeNum={"999"}
+        /> */}
 
         {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              หมาน้อย
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>2 คน</span>
-            </div>
-          </div>
-        </div>
+        {/* <Reply
+          userImagePath={"/profileImages/puppy.jpg"}
+          username={"หมาน้อย"}
+          replyText={"จริงค้าบบบบบบบบ"}
+          likeNum={"2"}
+        /> */}
 
         {/* map-loop render Comment component here */}
+        {comments.map((user) => (
+          <Comment
+            key={user.userImagePath}
+            userImagePath={user.userImagePath}
+            username={user.username}
+            commentText={user.commentText}
+            likeNum={user.likeNum}
+            replies={user.replies}
+          />
+        ))}
       </div>
     </div>
   );
